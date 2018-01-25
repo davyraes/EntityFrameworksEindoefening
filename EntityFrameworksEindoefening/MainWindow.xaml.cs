91f2dbdac26f9ab0b82d42dc5609936a4496a513
davyraes <davy-raes@telenet.be>
2018-01-25 12:11:38 +0100

    eindeoefening

--- EntityFrameworksEindoefening/MainWindow.xaml.cs
+++ EntityFrameworksEindoefening/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,56 @@ namespace EntityFrameworksEindoefening
     /// </summary>
     public partial class MainWindow : Window
     {
+        private LandenStedenTalenEntities entities = new LandenStedenTalenEntities();
         public MainWindow()
         {
             InitializeComponent();
+            VulList_landen();
+        }
+        private void VulList_landen()
+        {
+            List_Landen.ItemsSource = (from land in entities.Landen
+                                       orderby land.Naam
+                                       select land).ToList();
+        }
+        private void VulList_Steden()
+        {
+            if (List_Landen.SelectedItem!=null)
+                List_Steden.ItemsSource = ((Land)List_Landen.SelectedItem).Steden;
+        }
+        private void VulList_Talen()
+        {
+            if (List_Landen.SelectedItem != null)
+                List_Talen.ItemsSource = ((Land)List_Landen.SelectedItem).Talen;
+        }
+
+        private void List_Landen_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            VulList_Steden();
+            VulList_Talen();
+        }
+        
+        private int LaatstGekozen;
+        private void Button_bijvoegen_Click(object sender, RoutedEventArgs e)
+        {
+            if(Textbox_Stad.Text!=string.Empty&&Textbox_Stad.Text!=null)
+            {
+                if(List_Landen.SelectedItem!=null)
+                {
+                    LaatstGekozen = List_Landen.SelectedIndex;
+                    entities.Steden.Add(new Stad { Naam = Textbox_Stad.Text, Land = (Land)List_Landen.SelectedItem });
+                    entities.SaveChanges();
+                    Textbox_Stad.Text = string.Empty;
+                }
+                else
+                {
+                    MessageBox.Show("Er is geen land geselecteerd in de listbox");
+                }
+            }
+            else
+            {
+                MessageBox.Show("De textbox is leeg");
+            }
         }
     }
 }
